044ac3a3e2945501f51fc6c0feb30d5b2b98351c
Christopher <reinkendigital@gmail.com>
2020-04-08 12:49:10 -0500

    Done.

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,20 @@
+class Course
 
+    #### Attributes ####
+    attr_accessor :title, :schedule, :description
+    @@all = []
 
+    #### Instance Methods ####
+    def initialize
+        @@all << self
+    end
+
+    #### Class Methods####
+    def self.all
+        @@all
+    end
+
+    def self.reset_all
+        @@all.clear
+    end
+end
